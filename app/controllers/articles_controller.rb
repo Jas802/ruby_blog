d3574b6b2f74edda905419704684163d60995479
Jas802 <jas.spencer414@gmail.com>
2020-11-23 14:44:07 -0500

    create method with strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,13 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
     end
 
     def update
 
     end
+
 end
