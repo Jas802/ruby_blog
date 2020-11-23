7de54254e922016b25dbbcadc2a5f2b5d28d8976
Jas802 <jas.spencer414@gmail.com>
2020-11-23 15:22:16 -0500

    edit and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -22,7 +26,13 @@ class ArticlesController < ApplicationController
     end
 
     def update
-
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully!"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
 end
