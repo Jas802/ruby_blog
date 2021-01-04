c1dc9dd6f948c4fd2d6822e847d5fa566b2cd47e
Jas802 <jas.spencer414@gmail.com>
2021-01-04 13:31:15 -0500

    added user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
 
   def edit
     @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated your account information"
+      redirect_to articles_path
+    else
+    end
   end
 
   def create
@@ -27,6 +37,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
-
 end
