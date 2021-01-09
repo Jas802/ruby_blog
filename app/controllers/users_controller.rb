32a4d142c821cb7b539c247671e7cf8f234aa89d
Jas802 <jas.spencer414@gmail.com>
2021-01-09 11:40:51 -0500

    cleaned up links and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -9,7 +10,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -17,18 +17,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] = "You have updated your account information"
-      redirect_to @user
-    else
-    end
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Sign up Successful #{@user.username}!"
       redirect_to articles_path
     else
@@ -36,9 +28,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "You have updated your account information"
+      redirect_to @user
+    else
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
